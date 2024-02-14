321dea0f3301643c17db553d739f91ec6ae17527
satsuki <satsuki@localhost>
2024-02-14 23:29:15 +0900

    java1.23 提出

--- HelloWorld_HiroseMakoto/src/java1_23question/animalInfo.java
+++ HelloWorld_HiroseMakoto/src/java1_23question/animalInfo.java
@@ -3,16 +3,16 @@ package java1_23question;
 public class animalInfo {
 
 	public static void main(String[] args) {
-		info info = new info("ライオン",2.1,80);
-		System.out.println(info.toStirng());
+		info info = new info();
+		
+		// 変数に代入する値を引数に入れる
+		info.setAnimalName("ライオン");
+		info.setHeight(2.1);
+		info.setSpeed(80);
+		
+		// getterを用いて出力
+		System.out.println("動物名：" + info.getAnimalName());
+		System.out.println("体長：" + info.getHeight() + "m");
+		System.out.println("速度：" + info.getSpeed() + "km/h");
 	}
 }
-
-/*
-	下記がコンソールに出力されるように作成してください
-	※thisとsetterとgetterとフィールドを使ってください
-	
-	動物名：ライオン
-	体長：2.1m
-	速度：80km/h
-*/
--- HelloWorld_HiroseMakoto/src/java1_23question/info.java
+++ HelloWorld_HiroseMakoto/src/java1_23question/info.java
@@ -5,46 +5,26 @@ public class info {
 	private String animalName;
 	private Double height;
 	private Integer speed;
-	
-	// コンストラクタ
-	public info(String animalName,Double height,Integer speed){
-		// nullチェック
-		if((animalName != null) || (height != null) || (speed != null)) {
-			this.animalName = animalName;
-			this.height = height;
-			this.speed = speed;			
-		}
-	}
-	
-	// getterとsetter
-	public String getAnimalName() {
-		return animalName;
-	}
-	
+
+	// setter
 	public void setAnimalName(String animalName) {
 		this.animalName = animalName;
 	}
-	
-	public Double getHeight() {
-		return height;
-	}
-	
 	public void setHeight(double height) {
 		this.height = height;
 	}
-	
-	public Integer getSpeed() {
-		return speed;
-	}
-	
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 	
-	// 文字列を返す、animalInfoで出力する用
-	public String toStirng() {
-		return "動物名：" + animalName + "\n" 
-				+ "体長：" + height + "m" + "\n" 
-				+ "速度：" + speed + "km/h";
+	// getter
+	public String getAnimalName() {
+		return animalName;
+	}
+	public Double getHeight() {
+		return height;
+	}
+	public Integer getSpeed() {
+		return speed;
 	}
 }
