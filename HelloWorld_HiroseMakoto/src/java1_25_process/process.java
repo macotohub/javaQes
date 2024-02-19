a0ada8f44e047469d55e301cc9c1692dc3c639f0
satsuki <satsuki@localhost>
2024-02-19 12:09:28 +0900

    java1.25 途中

---
+++ HelloWorld_HiroseMakoto/src/java1_25_process/process.java
@@ -0,0 +1,98 @@
+// java1.25 question
+
+package java1_25_process;
+
+public class process {
+	private String name;
+	
+	public process() {
+	}
+	
+	public void user() {
+		System.out.println("こんにちは「 " + name + " 」さん");
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
+
+class status extends process{
+	private Integer hp;
+	private Integer mp;
+	private Integer power;
+	private Integer speed;
+	private Integer defence;
+	
+	public status() {	
+	}
+	
+	public void statusInfo() {
+		System.out.println("ステータス");
+		System.out.println("HP：" + getHp());
+		System.out.println("MP：" + getMp());
+		System.out.println("攻撃力：" + getPower());
+		System.out.println("素早さ：" + getSpeed());
+		System.out.println("防御力：" + getDefence());
+		System.out.println("\nさあ冒険に出かけよう！");
+	}
+		
+	public void setHp(Integer hp) {
+		this.hp = hp;
+	}
+	public void setMp(Integer mp) {
+		this.mp = mp;
+	}
+	public void setPower(Integer power) {
+		this.power = power;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public void setDefence(Integer defence) {
+		this.defence = defence;
+	}
+		
+		
+	public Integer getHp() {
+		return hp;
+	}
+	public Integer getMp() {
+		return mp;
+	}
+	public Integer getPower() {
+		return power;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public Integer getDefence() {
+		return defence;
+	}
+}
+
+
+/*
+名前を入力したら下記がコンソールに出力されるように作ってください
+	条件：数値は毎回変わるように作ってください
+	 	 サブクラスを使用してください
+		 スーパークラスを使用してください
+		 getterとsetterを使用してください
+		 packageを2つ作ってメインと処理を分けてください
+		 命名する場合は規則にのっとってください
+		 コンストラクタを使用してください
+
+	こんにちは 「 名前 」 さん
+	ステータス
+	HP：849
+	MP：862
+	攻撃力：375
+	素早さ：937
+	防御力：24
+
+	さあ冒険に出かけよう！
+*/
